aa5b306d0c6302d1081ed314dccbed852240825a
sAngello31 <bravoangello31@gmail.com>
2025-04-24 11:56:58 -0400

    Server setting

--- gateway/include/socket.h
+++ gateway/include/socket.h
@@ -1,8 +1,16 @@
 #ifndef SOCKET_H
 #define SOCKET_H
 
-#define DEFAULT_PORT 8080
+#include <stdio.h>
+#include <string.h>
+#include <sys/types.h>
+#include <sys/socket.h>
+#include <arpa/inet.h>
+#include <netdb.h>
 
+#define DEFAULT_PORT "8080"
 
+int open_listenfd(char *port);
+int error_connection(int connfd);
 
 #endif 
--- gateway/src/socket.c
+++ gateway/src/socket.c
@@ -1,3 +1,38 @@
-#include <stdio.h>
-
 #include "socket.h"
+
+int open_listenfd(char *port){
+    struct addrinfo hints, *listp, *p;
+    int listenfd, optval = -1;
+    memset(&hints, 0, sizeof(struct addrinfo));
+    hints.ai_socktype = SOCK_STREAM;
+    hints.ai_flags = AI_PASSIVE;
+    hints.ai_flags |= AI_NUMERICSERV;
+    hints.ai_flags |= AI_ADDRCONFIG;
+    getaddrinfo(NULL, port, &hints, &listp);
+
+    for(p = listp; p; p = p->ai_next){
+        if((listenfd = socket(p->ai_family, p->ai_socktype, p->ai_protocol)) < 0 ){
+            continue;
+        }
+        setsockopt(listenfd, SOL_SOCKET, SO_REUSEADDR,(const void*) &optval, sizeof(int));
+        
+        if(bind(listenfd, p->ai_addr, p->ai_addrlen) == 0){
+            break;
+        }
+        close(listenfd);
+    }
+    freeaddrinfo(listp);
+    if(!p){
+        return -1;
+    }
+    if(listen(listenfd, 1024) < 0){
+        return -1;
+    }
+    return listenfd;
+}
+
+int error_connection(int connfd){
+    fprintf(stderr, "Connection error: %s\n", strerror(connfd));
+    close(connfd);
+    return -1;
+}
